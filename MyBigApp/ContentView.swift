aac93737564a49cfba8d7cbb7bef720978e2d0b8
Ray Huang <rhuang@lcs.on.ca>
2026-03-02 15:13:34 -0500

    Almost finished with left and right

--- MyBigApp/ContentView.swift
+++ MyBigApp/ContentView.swift
@@ -8,17 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selection = "FlyLite"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            HStack {
+                //Left hand Right Hand
+                GeometryReader { GeometryProxy in
+                    Text("Hand")
+                        .font(Font.system(size: 25))
+                    
+                    //Left
+                    
+                    Text("L")
+                        .foregroundStyle(Color.white)
+                        .padding()
+                        .background {
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color.black)
+                                
+                            
+                        }
+                }
+            }
+        
+        //Space between Flylite and Hand
+           .padding()
+            //Top Left Title
+            .navigationTitle("Flylite")
+            //Back button
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        
+                    } label: {
+                        Text("\(Image(systemName: "chevron.backward"))")
+                        
+                    
+                    
+                    }
+                
+                
+                }
+                
+            }
         }
-        .padding()
     }
 }
 
+    
+    
 #Preview {
     ContentView()
 }
+
+
+
