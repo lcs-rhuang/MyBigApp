ca07160204d2a7515ae5ddf4ce34c8c20da5e99d
Ray Huang <rhuang@lcs.on.ca>
2026-03-04 09:46:59 -0500

    spoecifications

--- MyBigApp/MyBigApp2.swift
+++ MyBigApp/MyBigApp2.swift
@@ -24,34 +24,40 @@ struct MyBigApp2: View {
                     Spacer()
                         .padding()
                     
-                //Tesla Title
-                .navigationTitle("2022 Tesla Model X")
-                    Text("2022 Tesla Model X")
-                       
+                    //Tesla Title
                     
-                        
-                    
-                    
-                    
-                    
-                    
-                    // back button
-                        .toolbar {
-                            ToolbarItem(placement: .topBarLeading) {
-                                Button {
-                                    
-                                } label: {
-                                    Image(systemName: "chevron.backward")
-                                }
-                            }
-                        }
-                    
+                    HStack {
+                        Text("2022 Tesla Model X")
+                            .font(.system(size: 35, weight: .bold, design: .default))
+                        Spacer()
+                    }
                 }
-                
+                VStack {
+                    //blue text for 95k
+                    Text("$95,000.00")
+                        .font(.system(size: 25, weight: .bold, design: .default))
+                        .foregroundStyle(.blue)
+                    
+                    //Specification Text
+                }
+                    
+            
+                         
+                            // back button
+                                .toolbar {
+                                    ToolbarItem(placement: .topBarLeading) {
+                                        Button {
+                                            
+                                        } label: {
+                                            Image(systemName: "chevron.backward")
+                                        }
+                                }
+                        
+                    }
+                }
             }
         }
     }
-}
 
 #Preview {
     MyBigApp2()
