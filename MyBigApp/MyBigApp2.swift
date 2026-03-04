93c1ac53d52b753b21e47e66b93ebc5e7b0fed00
Ray Huang <rhuang@lcs.on.ca>
2026-03-04 10:03:07 -0500

    almost finished, jsut the picker

--- MyBigApp/MyBigApp2.swift
+++ MyBigApp/MyBigApp2.swift
@@ -37,13 +37,66 @@ struct MyBigApp2: View {
                     Text("$95,000.00")
                         .font(.system(size: 25, weight: .bold, design: .default))
                         .foregroundStyle(.blue)
-                    
+                    //space between 95k and specications
+                    Spacer()
                     //Specification Text
+                    Text("Specifications")
+                        .font(.system(size: 20, weight: .bold, design: .default))
+                        .padding()
+                        Spacer()
+            }
+                HStack {
+                    Text("Year")
+                        .foregroundStyle(.purple)
+                    Text("2022")
                 }
-                    
-            
-                         
-                            // back button
+                HStack {
+                    Text("Make")
+                        .foregroundStyle(.purple)
+                    Text("Tesla")
+                }
+                HStack {
+                    Text("Model")
+                        .foregroundStyle(.purple)
+                    Text("Model X")
+                }
+           
+                HStack {
+                    Text("Colour")
+                        .foregroundStyle(.purple)
+                    Text("Silver")
+                }
+           
+                HStack {
+                    Text("Drivetrain")
+                        .foregroundStyle(.purple)
+                    Text("All-wheel drive")
+                 
+                }
+           
+                HStack {
+                    Text("Listed on")
+                        .foregroundStyle(.purple)
+                    Text("May 22, 2024")
+                 
+                }
+                //car description
+                VStack {
+                    Text("Descriptions")
+                        .font(.system(size: 20, weight: .bold, design: .default))
+                        .padding()
+                    Text("Stunning Tesla Model X in immaculate condition with 22,000 kilometers of careful driving. This family-friendly SUV features the iconic falcon wing doors that operate flawlessly. 7 seat configuration with premium white interior, and...")
+                        .foregroundStyle(.purple)
+                 
+                        
+                }
+                //moving the whole thing up
+                Spacer()
+                Spacer()
+                Spacer()
+                
+                       
+                // back button
                                 .toolbar {
                                     ToolbarItem(placement: .topBarLeading) {
                                         Button {
